17f0b6022ec56c96b7c0d000768ca72ebdf1fe8a
Jammered <104011534+Jammered@users.noreply.github.com>
2022-06-05 17:44:10 +0100

    Add files via upload

---
+++ Buzzer_Testing_with_output.ino
@@ -0,0 +1,16 @@
+int duration = 500;
+
+void setup() {
+
+}
+
+void loop() {
+    tone(8, 1400, duration);
+    delay(200);
+    tone(8, 800, duration);
+    delay(200);
+    tone(8, 1800, duration);
+    delay(200);
+    tone(8, 600, duration);
+    delay(200);
+}
